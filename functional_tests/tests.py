e13da014451f1c26b9089b111db52a8587f7483d
lenovo of cuiyongfa <898953763@qq.com>
2019-03-03 23:42:51 +0800

    移出数据库文件

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,13 +4,28 @@ from selenium.common.exceptions import WebDriverException
 import time
 # from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
+import sys
 MAX_WAIT = 10
 
 import unittest
 
 
 class NewVistorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -35,7 +50,7 @@ class NewVistorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
 
     # 伊迪丝访问首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
     # 她看到输入框完美地居中显示
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -59,7 +74,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         #标题和头部都包含“待办事项”
         self.assertIn('待办事项', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -98,7 +113,7 @@ class NewVistorTest(StaticLiveServerTestCase):
         
         #新主页没有另一人的内容
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         time.sleep(1)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('买鱼饵', page_text)
