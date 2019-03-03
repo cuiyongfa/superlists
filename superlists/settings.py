dbab2498ca2c8913e023fef4a5d818d0e61eaa7a
lenovo of cuiyongfa <898953763@qq.com>
2019-03-03 21:04:21 +0800

    finish

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+s=o$+7u-($a)wa4q!-in1urbji@!2@b@83di5mm$b*q5-)%*h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
