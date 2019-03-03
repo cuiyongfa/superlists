e8e963146bdaf090360e115d38339d31fb4bf87a
lenovo of cuiyongfa <898953763@qq.com>
2019-03-03 09:12:05 +0800

    改动文件 2个迁移

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
-from django.urls import resolve
-from lists.views import home_page
-from django.http import HttpRequest
-from django.template.loader import render_to_string
+from lists.models import Item, List
 # Create your tests here.
 
 
@@ -13,26 +9,16 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-
-
-    # def test_display_all_list_items(self):
-    #     Item.objects.create(text='item 1')
-    #     Item.objects.create(text='item 2')
-    #
-    #     response = self.client.get('/')
-    #
-    #     self.assertIn('item 1', response.content.decode())
-    #     self.assertIn('item 2', response.content.decode())
-
     def test_only_saves_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
-    def test_dispaly_all_item(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+    def test_display_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)
 
         response = self.client.get('/lists/the_only_one_list_url/')
 
@@ -57,22 +43,34 @@ class NewListTest(TestCase):
         # self.assertEqual(response['location'], '/lists/the_only_one_list_urll')
         self.assertRedirects(response, '/lists/the_only_one_list_url/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_=List()
+        list_.save()
+
         first_item = Item()
+        first_item.list = list_
         first_item.text = '第一个条目'
         first_item.save()
 
         second_item = Item()
         second_item.text = '第二个条目'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item =saved_items[1]
+
         self.assertEqual(first_saved_item.text, '第一个条目')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '第二个条目')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 
