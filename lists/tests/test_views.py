16d09cf4cc9317eb1442eb7b1ec03f891574210a
lenovo of cuiyongfa <898953763@qq.com>
2019-03-07 19:23:08 +0800

    重组单元测试

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -77,40 +77,4 @@ class NewItemTest(TestCase):
 
 
 
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_=List()
-        list_.save()
-
-        first_item = Item()
-        first_item.list = list_
-        first_item.text = '第一个条目'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '第二个条目'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item =saved_items[1]
-
-        self.assertEqual(first_saved_item.text, '第一个条目')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '第二个条目')
-        self.assertEqual(second_saved_item.list, list_)
-
-
-
-
-
-
-
-
 
