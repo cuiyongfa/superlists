bbbd0859e6a81f18cdbf6e4e8e4cd3c7ddf2ae32
lenovo of cuiyongfa <898953763@qq.com>
2019-03-07 22:16:44 +0800

    创建新表输入空单显示错误

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -55,25 +55,39 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id, ))
 
-class NewItemTest(TestCase):
+    def test_validation_errors_can_sent_back_to_home_page(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expect_error = '输入不能为空'
+        self.assertContains(response, expect_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_new_item_can_save_a_POST_request_to_an_exiting_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
-        self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text': 'A new item for an existing list'})
+        self.client.post('/lists/%d/' % (correct_list.id,), data={'item_text': 'A new item for an existing list'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
-
         correct_list = List.objects.create()
-        response = self.client.post('/lists/%d/add_item' % (correct_list.id,),
+
+        response = self.client.post('/lists/%d/' % (correct_list.id,),
                                     data={'item_text': 'A new item for an existing list'})
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+
 
 
 
